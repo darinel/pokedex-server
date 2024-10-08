6bdb48bb606e50970bd8e19c8504ed2b43110bd6
darinel <darinelcm@yahoo.com>
2024-10-08 09:51:21 -0400

    Update2 pokedex.py
    Change number 2 to pokedex

--- pokedex.py
+++ pokedex.py
@@ -1,6 +1,7 @@
 import sqlite3
 from jsonparse import jsonParser
 # Change to be triggered with webhook
+# change number 2
 # JSON translation and parsing tables
 _namefields = ['english', 'japanese', 'chinese', 'french']
 _shortnames = ['en', 'jp', 'cn', 'fr']
